04d29483634c1d19dda5d06a06508b42bb6ac800
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-21 13:49:11 -0600

    Parameterized Batch Inserts

--- jdbc-app/src/main/java/com/loonycorn/DBUtils.java
+++ jdbc-app/src/main/java/com/loonycorn/DBUtils.java
@@ -7,6 +7,7 @@ import java.sql.SQLException; // class SQLException
 import javax.sql.rowset.JdbcRowSet; // interface JdbcRowSet
 import javax.sql.rowset.RowSetProvider; // class RowSetProvider
 import javax.sql.rowset.CachedRowSet; // interface CachedRowSet
+import java.sql.PreparedStatement; // interface PreparedStatement
 
 public class DBUtils {
 
@@ -72,6 +73,39 @@ public class DBUtils {
         }
 
         return cachedRS;
+    }
+
+    public static PreparedStatement getInsertVehiclePS(Connection conn) {
+
+        try {
+            return conn.prepareStatement("insert into delvehicles values(?, ?, ?, ?)");
+        }
+        catch (SQLException sqlex) {
+            sqlex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static boolean addToInsertVehicleBatch(PreparedStatement ps, int id,
+                                                  String color, String vehicleType,
+                                                  String lPlate) {
+        boolean addedQuery = false;
+
+        try {
+            ps.setInt(1, id);
+            ps.setString(2, color);
+            ps.setString(3, vehicleType);
+            ps.setString(4, lPlate);
+            ps.addBatch();
+
+            addedQuery = true;
+        }
+        catch (SQLException sqlex) {
+            sqlex.printStackTrace();
+        }
+
+        return addedQuery;
     }
 
 }
