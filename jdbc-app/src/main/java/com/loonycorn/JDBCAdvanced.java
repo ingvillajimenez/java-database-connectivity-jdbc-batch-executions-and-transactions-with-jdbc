ce48b6ba839ef1f45c97a96ef644dc98103a9e6b
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-21 17:58:31 -0600

    Implementing a Transaction

--- jdbc-app/src/main/java/com/loonycorn/JDBCAdvanced.java
+++ jdbc-app/src/main/java/com/loonycorn/JDBCAdvanced.java
@@ -6,77 +6,71 @@ import java.sql.Statement; // interface Statement
 
 public class JDBCAdvanced {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Statement stmnt;
         String query;
+        Connection conn = null;
 
-        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
+        try {
+
+            conn = DBUtils.getMysqlConnection("DeliveryService");
+            conn.setAutoCommit(false);
 
             stmnt = conn.createStatement();
 
             query = "insert into delpartners values (107, 'Kylie', 'Kass', 22.0, false)";
             stmnt.executeUpdate(query);
 
-            query = "insert into delvehicles values (16, 'Orange', 'Van', 'LOONY17')";
+            query = "insert into delvehicles values (17, 'Orange', 'Van', 'LOONY17')";
             stmnt.executeUpdate(query);
-            //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '16' for key 'delvehicles.PRIMARY'
-            //	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:117)
-            //	at com.mysql.cj.jdbc.exceptions.SQLExceptionsMapping.translateException(SQLExceptionsMapping.java:122)
-            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdateInternal(StatementImpl.java:1334)
-            //	at com.mysql.cj.jdbc.StatementImpl.executeLargeUpdate(StatementImpl.java:2084)
-            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdate(StatementImpl.java:1245)
-            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:22)
-            // the partner with 107 was inserted but her vehicle with id 16 was not so that it is an inconsistency
 
+            conn.commit();
             System.out.println("Rows have been successfully inserted");
+            //Rows have been successfully inserted
 
         }
         catch (SQLException ex) {
+            System.out.println("An exception was thrown. Rolling back the Tx...");
+            conn.rollback();
             ex.printStackTrace();
         }
+        finally {
+            conn.close();
+        }
 
-//        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
+//        try {
+//
+//            conn = DBUtils.getMysqlConnection("DeliveryService");
+//            conn.setAutoCommit(false);
 //
 //            stmnt = conn.createStatement();
 //
-//            query = "insert into delpartners values (106, 'Kylie', 'Kass', 22.0, false)";
+//            query = "insert into delpartners values (107, 'Kylie', 'Kass', 22.0, false)";
 //            stmnt.executeUpdate(query);
-//            //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '106' for key 'delpartners.PRIMARY'
+//
+//            query = "insert into delvehicles values (16, 'Orange', 'Van', 'LOONY17')";
+//            stmnt.executeUpdate(query);
+//            //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '16' for key 'delvehicles.PRIMARY'
 //            //	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:117)
 //            //	at com.mysql.cj.jdbc.exceptions.SQLExceptionsMapping.translateException(SQLExceptionsMapping.java:122)
 //            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdateInternal(StatementImpl.java:1334)
 //            //	at com.mysql.cj.jdbc.StatementImpl.executeLargeUpdate(StatementImpl.java:2084)
 //            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdate(StatementImpl.java:1245)
-//            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:19)
-//            // vehicle with id 17 was nos inserted because of the previous exception
+//            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:26)
 //
-//            query = "insert into delvehicles values (17, 'Orange', 'Van', 'LOONY17'";
-//            stmnt.executeUpdate(query);
-//
+//            conn.commit();
 //            System.out.println("Rows have been successfully inserted");
 //
 //        }
 //        catch (SQLException ex) {
+//            System.out.println("An exception was thrown. Rolling back the Tx...");
+//            //An exception was thrown. Rolling back the Tx...
+//            conn.rollback();
 //            ex.printStackTrace();
 //        }
-
-//        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
-//
-//            stmnt = conn.createStatement();
-//
-//            query = "insert into delpartners values (106, 'Pablo', 'Hernandez', 20.0, false)";
-//            stmnt.executeUpdate(query);
-//
-//            query = "insert into delvehicles values (16, 'Red', 'Truck', 'LOONY16')";
-//            stmnt.executeUpdate(query);
-//
-//            System.out.println("Rows have been successfully inserted");
-//            //Rows have been successfully inserted
-//
-//        }
-//        catch (SQLException ex) {
-//            ex.printStackTrace();
+//        finally {
+//            conn.close();
 //        }
 
     }
