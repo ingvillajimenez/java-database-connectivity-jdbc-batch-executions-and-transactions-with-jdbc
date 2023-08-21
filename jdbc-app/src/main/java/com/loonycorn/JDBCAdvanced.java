3e4fdf5b9e599e35933e7bb4ae89f6c5ab6aa641
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-21 16:00:51 -0600

    Running Inserts in a Sequence

--- jdbc-app/src/main/java/com/loonycorn/JDBCAdvanced.java
+++ jdbc-app/src/main/java/com/loonycorn/JDBCAdvanced.java
@@ -3,56 +3,81 @@ package com.loonycorn;
 import java.sql.Connection; // interface Connection
 import java.sql.SQLException; // class SQLException
 import java.sql.Statement; // interface Statement
-import java.util.Arrays; // class Arrays
-import java.sql.PreparedStatement; // interface PreparedStatement
 
 public class JDBCAdvanced {
 
     public static void main(String[] args) {
 
-        int[] count = {};
+        Statement stmnt;
+        String query;
 
         try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
 
-            PreparedStatement insertPS = DBUtils.getInsertVehiclePS(conn);
+            stmnt = conn.createStatement();
 
-            DBUtils.addToInsertVehicleBatch(insertPS, 20, "Yellow", "Truck", "LOONY20");
-            DBUtils.addToInsertVehicleBatch(insertPS, 19, "Pink", "Truck", "LOONY19");
-            DBUtils.addToInsertVehicleBatch(insertPS, 21, "Green", "Van", "LOONY21");
+            query = "insert into delpartners values (107, 'Kylie', 'Kass', 22.0, false)";
+            stmnt.executeUpdate(query);
 
-            count = insertPS.executeBatch();
-            //java.sql.BatchUpdateException: Duplicate entry '19' for key 'delvehicles.PRIMARY'
-            //	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
-            //	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)
-            //	at java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
-            //	at java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)
-            //	at java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)
-            //	at com.mysql.cj.util.Util.handleNewInstance(Util.java:192)
-            //	at com.mysql.cj.util.Util.getInstance(Util.java:167)
-            //	at com.mysql.cj.util.Util.getInstance(Util.java:174)
-            //	at com.mysql.cj.jdbc.exceptions.SQLError.createBatchUpdateException(SQLError.java:224)
-            //	at com.mysql.cj.jdbc.ClientPreparedStatement.executeBatchSerially(ClientPreparedStatement.java:816)
-            //	at com.mysql.cj.jdbc.ClientPreparedStatement.executeBatchInternal(ClientPreparedStatement.java:418)
-            //	at com.mysql.cj.jdbc.StatementImpl.executeBatch(StatementImpl.java:795)
-            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:23)
-            //Caused by: java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '19' for key 'delvehicles.PRIMARY'
+            query = "insert into delvehicles values (16, 'Orange', 'Van', 'LOONY17')";
+            stmnt.executeUpdate(query);
+            //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '16' for key 'delvehicles.PRIMARY'
             //	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:117)
             //	at com.mysql.cj.jdbc.exceptions.SQLExceptionsMapping.translateException(SQLExceptionsMapping.java:122)
-            //	at com.mysql.cj.jdbc.ClientPreparedStatement.executeInternal(ClientPreparedStatement.java:916)
-            //	at com.mysql.cj.jdbc.ClientPreparedStatement.executeUpdateInternal(ClientPreparedStatement.java:1061)
-            //	at com.mysql.cj.jdbc.ClientPreparedStatement.executeBatchSerially(ClientPreparedStatement.java:795)
-            //	... 3 more
-            // In spite of the exception, vehicles with id 20 and 21 were inserted into the table delvehicles
+            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdateInternal(StatementImpl.java:1334)
+            //	at com.mysql.cj.jdbc.StatementImpl.executeLargeUpdate(StatementImpl.java:2084)
+            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdate(StatementImpl.java:1245)
+            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:22)
+            // the partner with 107 was inserted but her vehicle with id 16 was not so that it is an inconsistency
 
-            System.out.println("Values inserted successfully: \n" + Arrays.toString(count));
+            System.out.println("Rows have been successfully inserted");
 
         }
         catch (SQLException ex) {
-            System.out.println("Inserts performed: \n" + Arrays.toString(count));
-            //Inserts performed:
-            //[]
             ex.printStackTrace();
         }
+
+//        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
+//
+//            stmnt = conn.createStatement();
+//
+//            query = "insert into delpartners values (106, 'Kylie', 'Kass', 22.0, false)";
+//            stmnt.executeUpdate(query);
+//            //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '106' for key 'delpartners.PRIMARY'
+//            //	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:117)
+//            //	at com.mysql.cj.jdbc.exceptions.SQLExceptionsMapping.translateException(SQLExceptionsMapping.java:122)
+//            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdateInternal(StatementImpl.java:1334)
+//            //	at com.mysql.cj.jdbc.StatementImpl.executeLargeUpdate(StatementImpl.java:2084)
+//            //	at com.mysql.cj.jdbc.StatementImpl.executeUpdate(StatementImpl.java:1245)
+//            //	at com.loonycorn.JDBCAdvanced.main(JDBCAdvanced.java:19)
+//            // vehicle with id 17 was nos inserted because of the previous exception
+//
+//            query = "insert into delvehicles values (17, 'Orange', 'Van', 'LOONY17'";
+//            stmnt.executeUpdate(query);
+//
+//            System.out.println("Rows have been successfully inserted");
+//
+//        }
+//        catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+
+//        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
+//
+//            stmnt = conn.createStatement();
+//
+//            query = "insert into delpartners values (106, 'Pablo', 'Hernandez', 20.0, false)";
+//            stmnt.executeUpdate(query);
+//
+//            query = "insert into delvehicles values (16, 'Red', 'Truck', 'LOONY16')";
+//            stmnt.executeUpdate(query);
+//
+//            System.out.println("Rows have been successfully inserted");
+//            //Rows have been successfully inserted
+//
+//        }
+//        catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
 
     }
 }
